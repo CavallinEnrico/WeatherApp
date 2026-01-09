c4941a49b8027a2b74946e0e81682f3a7d27d07a
Enrico Cavallin <enricocavallin08@gmail.com>
2026-01-09 23:15:03 +0100

    Finalized navbar design with logos and links, functioning clock and ALMOST full responsiveness

--- assets/code/js/detailed.js
+++ assets/code/js/detailed.js
@@ -1,6 +1,29 @@
 // toggle theme
-const toggleThemeButton = document.querySelector(".toggle-theme")
+const toggleThemeButton = document.querySelector("#toggle-theme")
 
 toggleThemeButton.addEventListener("click", () => {
     document.body.classList.toggle("light")
+    if(document.body.classList.contains("light")) {
+        toggleThemeButton.src = "../downloads/logos/moon.svg"
+    } else {
+        toggleThemeButton.src = "../downloads/logos/sun.svg"
+    }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    updateClock();
+})
+
+
+
+
+function updateClock() {
+    const clockElement = document.querySelector(".clock");
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    clockElement.textContent = `${hours}:${minutes}`;
+}
+
+setInterval(updateClock, 3000);
+
