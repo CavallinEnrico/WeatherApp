14be12f13d9c976f221756046cd84676612cde8a
Enrico Cavallin <enricocavallin08@gmail.com>
2026-01-15 09:18:25 +0100

    Adding real weather data logic

--- assets/code/js/detailed.js
+++ assets/code/js/detailed.js
@@ -1,3 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search)
+const lat = urlParams.get('lat')
+const lon = urlParams.get('lon')
+
 // toggle theme
 const toggleThemeButton = document.querySelector("#toggle-theme")
 
@@ -17,19 +21,52 @@ toggleThemeButton.addEventListener("click", () => {
 })
 
 document.addEventListener("DOMContentLoaded", () => {
-    updateClock();
+    updateClock()
+    fetchDailyWeather(0)
 })
 
 
 
 
 function updateClock() {
-    const clockElement = document.querySelector(".clock");
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    clockElement.textContent = `${hours}:${minutes}`;
+    const clockElement = document.querySelector(".clock")
+    const now = new Date()
+    const hours = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    clockElement.textContent = `${hours}:${minutes}`
 }
 
 setInterval(updateClock, 3000);
 
+async function fetchDailyWeather(dateDiff) {
+    const date = formatDate(dateDiff)
+    const weatherData = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m,weather_code&start_date=${date}&end_date=${date}`)
+    const weatherJson = await weatherData.json()
+    const currentData = weatherJson.current
+    const dailyData = weatherJson.daily
+
+    document.querySelector("#temp-value").innerText = `${currentData.temperature_2m}°C`
+    document.querySelector("#humidity-value").innerText = `${currentData.relative_humidity_2m}%`
+    document.querySelector("#precipitations-value").innerText = `${currentData.precipitation}mm`
+    presentationData = await fetchPresentation(currentData.precipitation, currentData.temperature_2m, currentData.apparent_temperature, dailyData.temperature_2m_max[0], dailyData.temperature_2m_min[0], currentData.relative_humidity_2m, currentData.wind_speed_10m, currentData.wind_direction_10m)
+    console.log(currentData.windDirection_10m);
+}
+
+async function fetchPresentation(prec, temp, feelsLike, maxTemp, minTemp, humidity, windSpeed, windDir) {
+    const presentationData = await fetch(`https://weather-app-backend-mu-weld.vercel.app/api/generate_response?precipitationsStr=${prec}&actual_temperatureStr=${temp}&feels_like_temperatureStr=${feelsLike}&max_temperatureStr=${maxTemp}&min_temperatureStr=${minTemp}&humidityStr=${humidity}&wind_speedStr=${windSpeed}&wind_direction=NE`, {method: "GET"})
+    const presentationJson = await presentationData.json()
+    console.log("CIAO")
+    document.querySelector("#presentation-paragraph").innerText = presentationJson.response
+}
+
+function formatDate(diff) {
+    let returnDate = ""
+    const date = new Date()
+    date.setDate(date.getDate() + diff)
+    returnDate = date.getFullYear()+"-"
+    if(date.getMonth()+1 < 10)
+        returnDate += "0"
+    returnDate += (date.getMonth()+1)
+    returnDate += "-"+date.getDate()
+    return returnDate
+}
